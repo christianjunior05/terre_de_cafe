b9b89a1d53a9a95fe7e6e7bcd911ada1e98f82ab
Christian Junior Braffo <104594191+christianjunior05@users.noreply.github.com>
2023-10-08 19:09:04 +0000

    add

--- contact.php
+++ contact.php
@@ -24,61 +24,81 @@
 
       <main>
 
-        <section class="contact_section sec_ptb_120 bg_default_gray" id="contact">
-          <div class="container">
-            <div class="contact_form bg_white wow fadeInUp" data-wow-delay=".1s">
-              <div class="main_contact_info_wrap">
-                <div class="contact_info_item">
-                  <div class="item_icon"><i class="fa-solid fa-envelope"></i></div>
-                  <div class="item_content">
-                    <h3 class="item_title text-uppercase">Adresse email</h3>
-                    <p>Israfilsupol836@gmail.com</p>
-                    <p>Info@webmail@gmail.com</p>
-                  </div>
-                </div>
-                <div class="contact_info_item">
-                  <div class="item_icon"><i class="fa-solid fa-location-dot"></i></div>
-                  <div class="item_content">
-                    <h3 class="item_title text-uppercase">Notre localisation</h3>
-                    <p>Hera Road 2344-78 Australia</p>
-                    <p>897- South Side Melbon</p>
-                  </div>
-                </div>
-                <div class="contact_info_item">
-                  <div class="item_icon"><i class="fa-solid fa-phone"></i></div>
-                  <div class="item_content">
-                    <h3 class="item_title text-uppercase">Numéro de telephone</h3>
-                    <p>088 - 01869018907</p>
-                    <p>(88) 908 675 43667</p>
-                  </div>
-                </div>
-              </div>
-              <form action="#">
-                <div class="row">
-                  <div class="col-lg-6">
-                    <div class="form_item">
-                      <input type="text" name="name" placeholder="Your name:">
-                    </div>
-                  </div>
-                  <div class="col-lg-6">
-                    <div class="form_item">
-                      <input type="email" name="email" placeholder="Your Mail:">
-                    </div>
-                  </div>
-                </div>
-                <div class="form_item">
-                  <input type="text" name="subject" placeholder="Enter Your Subject:">
-                </div>
-                <div class="form_item">
-                  <textarea name="message" placeholder="Your Massage:"></textarea>
-                </div>
-                <div class="text-center">
-                  <button type="submit" class="btn btn_primary text-uppercase">Envoyer Le message</button>
-                </div>
-              </form>
+      Bien sûr, vous souhaitez améliorer la partie des cases à cocher (checkbox) dans le formulaire. Je vais utiliser des éléments Bootstrap pour améliorer la présentation des cases à cocher. Voici le code HTML amélioré :
+
+```html
+<section class="contact_section sec_ptb_120 bg_default_gray" id="contact">
+  <div class="container">
+    <div class="contact_form bg_white wow fadeInUp" data-wow-delay=".1s">
+      <div class="main_contact_info_wrap">
+        <!-- Vos informations de contact -->
+      </div>
+      <form action="#">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="form_item">
+              <input type="text" name="name" placeholder="Your name:">
             </div>
           </div>
-        </section>
+          <div class="col-lg-6">
+            <div class="form_item">
+              <input type="email" name="email" placeholder="Your Mail:">
+            </div>
+          </div>
+        </div>
+        <div class="form_item">
+          <input type="text" name="subject" placeholder="Enter Your Subject:">
+        </div>
+        
+        <fieldset class="mb-3">
+          <legend>Votre secteur d'activité</legend>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="__field_29029" value="Bureaux & entreprises" id="sector_bureaux">
+            <label class="form-check-label" for="sector_bureaux">
+              Bureaux & entreprises
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="__field_29029" value="Hôtel camping & gite" id="sector_hotel">
+            <label class="form-check-label" for="sector_hotel">
+              Hôtel, camping & gite
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="__field_29029" value="Etablissement de santé" id="sector_sante">
+            <label class="form-check-label" for="sector_sante">
+              Etablissement de santé
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="__field_29029" value="Café & restaurant" id="sector_cafe">
+            <label class="form-check-label" for="sector_cafe">
+              Café & restaurant
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="__field_29029" value="Ecole & université" id="sector_ecole">
+            <label class="form-check-label" for="sector_ecole">
+              Ecole & université
+            </label>
+          </div>
+        </fieldset>
+
+        <div class="form_item">
+          <textarea name="message" placeholder="Your Massage:"></textarea>
+        </div>
+        <div class="text-center">
+          <button type="submit" class="btn btn_primary text-uppercase">Envoyer Le message</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
+
 
         
       <?php include_once 'includes/footer.php'; ?>
