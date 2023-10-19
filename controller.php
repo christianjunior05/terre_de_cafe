857cffd9aaf418cce3e042f80226390fb77adc91
Christian Junior Braffo <104594191+christianjunior05@users.noreply.github.com>
2023-10-19 08:31:43 +0000

    db

--- controller.php
+++ controller.php
@@ -5,24 +5,25 @@ include_once 'config.php';
 if (isset($_POST['validez'])) {
     if (!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['adresse']) and !empty($_POST['phone']) and !empty($_POST['objet'])) {
         $jour = date("Y-m-d");
-        $name = htmlspecialchars($_POST['name']);
+        $name = htmlspecialchars($_POST['nom']);
         $email = htmlspecialchars($_POST['email']);
-        $adresse = htmlspecialchars($_POST['adresse']);
-        $phone = htmlspecialchars($_POST['phone']);
-        $objet = htmlspecialchars($_POST['objet']);
+        $name_entreprise = htmlspecialchars($_POST['nom_entreprise']);
+        $function = htmlspecialchars($_POST['function']);
+        $secteur = htmlspecialchars($_POST['secteur']);
+        $conso = htmlspecialchars($_POST['conso']);
 
         $pattern = '/^\+?[0-9\s.-]+$/';
         if (preg_match($pattern, $phone)) {
             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $ajout_entreprise = $conn->prepare('INSERT INTO entreprises (jour, rs, mail, adresse, tel, objet) VALUES (?, ?, ?, ?, ?, ?)');
+                $ajout_entreprise = $conn->prepare('INSERT INTO contact (jour, rs, mail, adresse, tel, objet) VALUES (?, ?, ?, ?, ?, ?)');
                 $ajout_entreprise->execute(array($jour, $name, $email, $adresse, $phone, $objet));
                 header('Location: reponse.php');
-                $_POST['jour'] = "";
-                $_POST['name'] = "";
-                $_POST['adresse'] = "";
+                $_POST['nom'] = "";
                 $_POST['email'] = "";
-                $_POST['phone'] = "";
-                $_POST['objet'] = "";
+                $_POST['nom_entreprise'] = "";
+                $_POST['function'] = "";
+                $_POST['secteur'] = "";
+                $_POST['conso'] = "";
             } else {
                 $er_mail = "adresse mail invalide";
             }
