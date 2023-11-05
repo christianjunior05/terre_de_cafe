c29a1b673892f59c9cf77fbbee3f3d5cd8fd566d
christianjunior05 <bchrist.junior@gmail.com>
2023-11-05 19:28:47 +0000

    add

--- about.php
+++ about.php
@@ -1,199 +1,78 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
-<?php include_once 'includes/header.php'; ?>
-<?php include_once 'includes/navbar.php'; ?>
-
-
-  <body>
-
-    <!-- body_wrap - start -->
+<head>
+    <?php include_once 'includes/header.php'; ?>
+    <?php include_once 'includes/navbar.php'; ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
     <div class="body_wrap">
-
-      <!-- backtotop - start -->
-      <div class="backtotop">
-        <a href="#" class="scroll">
-          <i class="far fa-arrow-up"></i>
-          <i class="far fa-arrow-up"></i>
-        </a>
-      </div>
-      <!-- backtotop - end -->
-
-      <!-- preloader - start -->
-      <div id="preloader"></div>
-      <!-- preloader - end -->
-
-      <main>
-
-
-
-        <!-- breadcrumb_section - start
-        ================================================== -->
-        <section class="breadcrumb_section text-uppercase" style="background-image: url(assets/images/breadcrumb/5.jpg) ;">
-          <div class="container">
-            <h1 class="page_title text-white wow fadeInUp" data-wow-delay=".1s">A Propos</h1>
-            <ul class="breadcrumb_nav ul_li wow fadeInUp" data-wow-delay=".2s">
-
-            
-              <li>A Propos</li>
-            </ul>
-          </div>
-          <div class="breadcrumb_icon_wrap">
-            <div class="container">
-              <div class="breadcrumb_icon wow fadeInUp" data-wow-delay=".3s">
-                <img src="assets/images/feature/icon_01.png" alt="icon_not_found">
-                <span class="bg_shape"></span>
-              </div>
-            </div>
-          </div>
-        </section>
-        <!-- breadcrumb_section - end
-        ================================================== -->
-
-        <!-- about_section - start
-        ================================================== -->
-        <section class="about_section sec_ptb_120">
-          <div class="container">
-            <div class="row align-items-center justify-content-lg-between">
-              <div class="col-lg-6 col-md-6 order-last">
-               
-              </div>
-
-              <div class="col-lg-5 col-md-5">
-                <div class="about_content">
-                  <div class="section_title text-uppercase">
-                    <h3 class="big_title wow fadeInUp" data-wow-delay=".2s">
-                      <u>5 ans d'experience</u>
-                    </h3>
-                  </div>
-                  <p class="wow fadeInUp" data-wow-delay=".3s">
-                  Chez Terre de café, nous sommes animés depuis plus de 5 ans par notre passion pour le café ,
-                  notre respect de l'environnement et notre souci des personnes.
-                  Nous agissons pour minimiser notre empreinte écologique tout en développant notre implication
-                  dans nos communautés pour un avenir plus durable et plus solidaire.
-                  </p>
-
+        <div class="backtotop">
+            <a href="#" class="scroll">
+                <i class="far fa-arrow-up"></i>
+                <i class="far fa-arrow-up"></i>
+            </a>
+        </div>
+        <div id="preloader"></div>
+        <main>
+          
+            <section class="about_section sec_ptb_120">
+                <div class="container">
+                    <div class="row align-items-center justify-content-lg-between">
+                        <div class="col-lg-6 col-md-6 order-last">
+                            
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                        <h1>Bienvenue sur TERREDECAFE</h1>
+                        <br>
+                            <div class="about_content">
+                                <div class="section_title text-uppercase">
+                                    <h3  data-wow-delay=".2s">
+                                        Depuis 2018
+                                    </h3>
+                                </div>
+                                <p class="wow fadeInUp" data-wow-delay=".3s">
+                                    Chez Terre de café,  nous sommes persuadés que déguster du café de qualité sur son lieu de travail, c’est possible. Et surtout c’est beaucoup mieux, tant pour la planète que pour les salariés.
+                                    C’est pour cette raison que nous nous sommes donné pour mission de transformer la pause-café pour en faire un lieu de vie. Parce que le café c’est plus qu’un simple breuvage : c’est une formidable occasion de nourrir liens avec les autres.
+                                </p>
+                                <div class="section_title text-uppercase">
+                                    <h3  data-wow-delay=".2s">
+                                        Notre mission : transformer la pause-café en entreprise
+                                    </h3>
+                                </div>
+                                <p class="wow fadeInUp" data-wow-delay=".3s">
+                                    Pour en faire un lieu de vie et un rituel nourrissant les liens avec les autres et la planète.
+                                    Nous pensons qu’un excellent café au bureau est une formidable bonne nouvelle, l’étincelle des matins blafards, un catalyseur à idées qui rassemble les gens, stimule leur créativité, anime les conversations et fluidifie les relations.
+                                </p>
+                                <div class="section_title text-uppercase">
+                                    <h3  data-wow-delay=".2s">
+                                        Notre But et Nos Attentes
+                                    </h3>
+                                </div>
+                                <p class="wow fadeInUp" data-wow-delay=".3s">
+                                    Notre but est de créer un environnement de travail convivial où le café est plus qu'une boisson, c'est un moyen de connecter les gens, de stimuler la créativité et d'améliorer la qualité de vie au bureau. Nous voulons faire de la pause-café un moment précieux.
+                                </p>
+                                <p class="wow fadeInUp" data-wow-delay=".3s">
+                                    Nous attendons que notre engagement envers la qualité, la durabilité et la satisfaction du client dépasse les attentes. Nous cherchons à être une source de motivation et d'inspiration pour d'autres entreprises cherchant à créer des environnements de travail positifs.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-
-            <div class="col-lg-5 col-md-5">
-                <div class="about_content">
-                  <div class="section_title text-uppercase">
-                    <h3 class="big_title wow fadeInUp" data-wow-delay=".2s">
-                      <u>Machine à café Elin</u>
-                    </h3>
-                  </div>
-                  <p class="wow fadeInUp" data-wow-delay=".3s">
-                    <h4>Elin est une machine à café haut de gamme réunissant style et technologie pour une pause-café riche en goût.</h4>
-                    Moderne, compacte et très performante elle s’illustre à tous les niveaux pour vous offrir le meilleur des cafés, facilement au bout de vos doigts. Avec une capacité de 100 tasses par jour, elle séduit par sa qualité et sa fiabilité et trouvera sa place dans les espaces de pause de toutes les entreprises. 
-                    Vos collaborateurs seront conquis.
-                  </p>
-                </div>
-              </div>
-            </div>
-      
-            <div class="row">
-              <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="feature_primary wow fadeInUp" data-wow-delay=".1s">
-                  <div class="item_icon">
-                    <span class="item_serial">1</span>
-                    <img src="assets/images/feature/icon_01.png" alt="icon_not_found">
-                  </div>
-                  <h3 class="item_title text-uppercase">Arome exceptionnelle</h3>
-                  <p class="mb-0">
-                   Café aux aromes naturels 
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="feature_primary wow fadeInUp" data-wow-delay=".2s">
-                  <div class="item_icon">
-                    <span class="item_serial">2</span>
-                    <img src="assets/images/feature/icon_02.png" alt="icon_not_found">
-                  </div>
-                  <h3 class="item_title text-uppercase">Qualité supérieure<h3>
-                   Un café d'excellente qualité
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="feature_primary wow fadeInUp" data-wow-delay=".3s">
-                  <div class="item_icon">
-                    <span class="item_serial">3</span>
-                    <img src="assets/images/feature/icon_03.png" alt="icon_not_found">
-                  </div>
-                  <h3 class="item_title text-uppercase">pure grains</h3>
-                  <p class="mb-0">
-                   Un café cultivé dans les milleurs conditions 
-                  </p>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="feature_primary wow fadeInUp" data-wow-delay=".4s">
-                  <div class="item_icon">
-                    <span class="item_serial">4</span>
-                    <img src="assets/images/feature/icon_04.png" alt="icon_not_found">
-                  </div>
-                  <h3 class="item_title text-uppercase">gout supérieur</h3>
-                  <p class="mb-0">
-                   Un café au gout supérieur
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <!-- about_section - end
-        ================================================== -->
-
-        <!-- testimonial_section - end
-        ================================================== -->
-
-      </main>
-      <!-- main body - end
-      ================================================== -->
-
-      <!-- footer_section - start
-      ================================================== -->
-      <?php include_once 'includes/footer.php'; ?>
-      <!-- footer_section - end
-      ================================================== -->
-
+            </section>
+        </main>
+        <?php include_once 'includes/footer.php'; ?>
     </div>
-    <!-- body_wrap - end -->
-
-    <!-- fraimwork - jquery include -->
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-
-    <!-- animation - jquery include -->
     <script src="assets/js/wow.min.js"></script>
-
-    <!-- carousel - jquery include -->
     <script src="assets/js/slick.min.js"></script>
-
-    <!-- popup - jquery include -->
     <script src="assets/js/magnific-popup.min.js"></script>
-
-    <!-- isotope filter - jquery include -->
     <script src="assets/js/isotope.pkgd.min.js"></script>
-
-    <!-- google map - jquery include -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDk2HrmqE4sWSei0XdKGbOMOHN3Mm2Bf-M&ver=2.1.6"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&ver=2.1.6"></script>
     <script src="assets/js/gmaps.min.js"></script>
-
-    <!-- jquery-ui - jquery include -->
     <script src="assets/js/jquery-ui.js"></script>
-
-    <!-- off canvas sidebar - jquery include -->
     <script src="assets/js/mCustomScrollbar.js"></script>
-
-    <!-- custom - jquery include -->
     <script src="assets/js/main.js"></script>
-
-  </body>
+</body>
 </html>
